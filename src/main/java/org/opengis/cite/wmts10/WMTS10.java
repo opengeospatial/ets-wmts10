9df753e8a506c35f863f25d23b843353692da274
Keshav Nangare <keshav.nangare@mindstix.com>
2018-10-12 22:18:02 +0530

    Invoked the test via REST API.

---
+++ src/main/java/org/opengis/cite/wmts10/WMTS10.java
@@ -0,0 +1,8 @@
+package org.opengis.cite.wmts10;
+
+public class WMTS10 {
+
+	/** Supplies reference to WMTS capabilities document (URI). */
+    public static final String CAPABILITIES_URL = "capabilities-url";
+	
+}
